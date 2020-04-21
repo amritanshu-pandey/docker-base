6169d133168599848e4059512ab8d77edb1a3de1
Amritanshu Pandey <amp.msit@gmail.com>
2020-04-21 20:09:31 +1000

    Add baked initialise script

---
+++ root/etc/baked-initialise.sh.d/00-motd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "This image is based upon amritanshu16/docker-base."
--- root/initialise.sh
+++ root/initialise.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-for init_script in $(find /etc/initialise.sh.d -type f | sort); do
+for init_script in $(find /etc/baked-initialise.sh.d /etc/initialise.sh.d -type f | sort); do
     echo "Executing init script: ${init_script}"
     "${init_script}"
     echo "Finished execution of init script: ${init_script}"
